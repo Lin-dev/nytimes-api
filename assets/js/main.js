f338c94bb99c18a43043866922a2b9ca9388230b
bklynate <nathanielcfa@gmail.com>
2016-12-17 13:58:12 -0500

    work

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
-var url = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q="' + queryItem + '"&sort=newest&&api-key=0ae332e50b7a4da8a9a8c64c50efbbec'
+var queryItem = $("#search-term").val();
 
-var queryItem = $("#term").val()
+var url = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q="' + queryItem + '"&sort=newest&&api-key=0ae332e50b7a4da8a9a8c64c50efbbec'
 
 console.log(queryItem);
 
@@ -9,7 +9,7 @@ $.ajax({
   method: 'GET'
 }).done(function(response){
   res = response;
-  for(var i = 0; i < res.response.docs.length; i++){
-    console.log(res.response.docs[i].headline.main)
-  }
+  // for(var i = 0; i < res.response.docs.length; i++){
+  //   console.log(res.response.docs[i].headline.main)
+  // }
 });
