d1a9070f274b7f738c46505f601b96322208af68
bklynate <nathanielcfa@gmail.com>
2016-12-17 13:32:46 -0500

    linked js file

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+var url = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q="' + queryItem + '"&sort=newest&&api-key=0ae332e50b7a4da8a9a8c64c50efbbec'
+
+var queryItem = $("#term").val()
+
+console.log(queryItem);
+
+$.ajax({
+  url: url,
+  method: 'GET'
+}).done(function(response){
+  res = response;
+  for(var i = 0; i < res.response.docs.length; i++){
+    console.log(res.response.docs[i].headline.main)
+  }
+});
