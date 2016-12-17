faa43c57807db49521b2fe09fde96231934fabe2
bklynate <nathanielcfa@gmail.com>
2016-12-17 17:19:37 -0500

    added comments to the javascript file

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,21 @@
+// this attachs a click event to ALL buttons on the page
 $('button').on('click', function(e) {
+
+  // this stops stops the default behavior
+  // and stops the page from reloading due to the submit buttons
+  // default behavior.
   e.preventDefault()
+
+  // this line grabs the value of the search input
   var queryItem = $("#search").val();
 
+  // this is the api call with the queryItem
+  // concatenated into the string
   var url = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q="' + queryItem + '"&sort=newest&&api-key=0ae332e50b7a4da8a9a8c64c50efbbec';
 
   console.log(queryItem);
 
+  // start of the Ajax call
   $.ajax({
     url: url,
     method: 'GET'
@@ -13,14 +23,17 @@ $('button').on('click', function(e) {
     console.log(response);
     res = response;
     $("#results").empty();
+    // for the number of results returned
     for(var i = 0; i < res.response.docs.length; i++){
       console.log(res.response.docs[i].headline.main)
+      // create a new element
       articleElements = $("<p>");
       hr = $('<hr/>');
+      // make the text of the element the main headline
       articleElements.text(res.response.docs[i].headline.main)
       articleElements.append(hr)
+      // attachs the results of the iteration to the 'results' div
       $('#results').prepend(articleElements)
     }
   });
-
 })
